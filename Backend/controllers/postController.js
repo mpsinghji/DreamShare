75aeb9030b756c732f3d1c99d28960ddc7ca21aa
Manpreet Singh <Manpreet.singhcomet@gmail.com>
2025-04-07 15:59:38 +0530

    get all posts done

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -1,44 +1,82 @@
 import catchAsync from "../utils/catchAsync.js";
+import Post from "../models/postModel.js";
+import User from "../models/userModel.js";
+import AppError from "../utils/appError.js";
+import sharp from "sharp";
+import { uploadToCloudinary } from "../utils/cloudinary.js";
+import { getDataUri } from "../utils/dataUri.js";
 
 export const createPost = catchAsync(async (req, res, next) => {
-    const { caption } = req.body;
-    const image = req.file;
-    const userId = req.user.id;
+  const { caption } = req.body;
+  const image = req.file;
+  const userId = req.user.id;
 
-    if(!image){
-        return next(new AppError("Image is required", 400));
-    }
+  if (!image) {
+    return next(new AppError("Image is required", 400));
+  }
 
-    const optimizedImageBuffer = await sharp(image.buffer).resize({
-        width: 800,
-        height: 800,
-        fit: "inside",
-    }).toFormat("jpeg", { quality: 80 }).toBuffer();
+  const optimizedImageBuffer = await sharp(image.buffer)
+    .resize({
+      width: 800,
+      height: 800,
+      fit: "inside",
+    })
+    .toFormat("jpeg", { quality: 80 })
+    .toBuffer();
 
-    const fileUri = `data:image/jpeg;base64,${optimizedImageBuffer.toString("base64")}`;
-    const cloudResponse = await uploadToCloudinary(fileUri);
-    
-    let post = await Post.create({
-        caption,
-        image: {
-            url: cloudResponse.secure_url,
-            public_id: cloudResponse.public_id,
-        },
-        user: userId,
-    });
-    
-    const user = await User.findById(userId);
-    if(user){
-        user.posts.push(post._id);
-        await user.save({validateBeforeSave: false});
-    }
+  const fileUri = `data:image/jpeg;base64,${optimizedImageBuffer.toString(
+    "base64"
+  )}`;
+  const cloudResponse = await uploadToCloudinary(fileUri);
 
-    post = await post.populate({path: "user", select: "username profilePicture email bio"});
+  let post = await Post.create({
+    caption,
+    image: {
+      url: cloudResponse.secure_url,
+      public_id: cloudResponse.public_id,
+    },
+    user: userId,
+  });
 
-    res.status(201).json({
-        status: "success",
-        message: "Post created successfully",
-        data: post,
-    });
-    
+  const user = await User.findById(userId);
+  if (user) {
+    user.posts.push(post._id);
+    await user.save({ validateBeforeSave: false });
+  }
+
+  post = await post.populate({
+    path: "user",
+    select: "username profilePicture email bio",
+  });
+
+  res.status(201).json({
+    status: "success",
+    message: "Post created successfully",
+    data: post,
+  });
+});
+
+export const getPosts = catchAsync(async (req, res, next) => {
+  const posts = await Post.find()
+    .populate({
+      path: "user",
+      select: "username profilePicture bio",
+    })
+    .populate({
+      path: "comments",
+      select: "text user",
+      populate: {
+        path: "user",
+        select: "username profilePicture",
+      },
+    })
+    .sort({ createdAt: -1 });
+
+  return res.status(200).json({
+    status: "success",
+    results: posts.length,
+    data: {
+      posts,
+    },
+  });
 });
--- Backend/routes/postRoutes.js
+++ Backend/routes/postRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPost } from "../controllers/postController.js";
+import { createPost, getPosts } from "../controllers/postController.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 import upload from "../middleware/multer.js";
 
 const router = express.Router();
 
 router.post("/create-post", isAuthenticated, upload.single("image"), createPost);
+router.get("/all", getPosts);
 
 export default router;
